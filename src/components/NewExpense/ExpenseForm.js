5ebf1eb038c66b285aa15fddfcfe9fb19614e92e
AliTosJM <alitosjm@gmail.com>
2021-06-01 18:17:34 -0500

    3 more onChange

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,6 +17,17 @@ function ExpenseForm(){
         //   return { ...prevState, enteredTitle: event.target.value };
         // });
       };
+    
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        console.log(event.target.value)
+      };
+    
+    
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        console.log(event.target.value)
+      };
 
     return(
         <form>
@@ -27,11 +38,11 @@ function ExpenseForm(){
                 </div>
                 <div className='new-expense__control'>
                     <label>Amout</label>
-                    <input type='number' min='0.01' step='0.01'/>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2020-01-01' max='2023-12-31'/>
+                    <input type='date' min='2020-01-01' max='2023-12-31' onChange={dateChangeHandler}/>
                 </div>
                 <div className='new-expense__actions'>
                     <button type='submit'>Add Expense</button>
