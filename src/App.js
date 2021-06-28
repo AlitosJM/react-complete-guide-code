094321e90e7a8f840696cb5a818a8d47e14a0b2c
AliTosJM <alitosjm@gmail.com>
2021-06-28 03:26:39 -0500

    Context component

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(()=>{
-    const storeUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-    if(storeUserLoggedInInfo==="1"){
+    if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
-      console.log("useEffect if")
-    }
-    else {
-      console.log("useEffect else")
     }
   }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
@@ -32,13 +29,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,28 +1,37 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(cxt) => 
+        {
+          return(
+            <nav className={classes.nav}>
+              <ul>
+                {cxt.isLoggedIn && (
+                  <li>
+                    <a href="/">Users</a>
+                  </li>
+                )}
+                {cxt.isLoggedIn && (
+                  <li>
+                    <a href="/">Admin</a>
+                  </li>
+                )}
+                {cxt.isLoggedIn && (
+                  <li>
+                    <button onClick={props.onLogout}>Logout</button>
+                  </li>
+                )}
+              </ul>
+            </nav>
+          );
+        }
+      }
+    </AuthContext.Consumer>
   );
 };
 
