a411d5d285c600803d83bdd2899395e9c44284df
AliTosJM <alitosjm@gmail.com>
2021-06-30 01:16:02 -0500

    almost the nav

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+// classes["header"] or classes.header
+
+const Header = props =>{
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes["main-image"]}>
+            <img src={mealsImage} alt="A table of delicious food!"/>
+        </div>
+    </React.Fragment>
+
+}
+
+export default Header;
