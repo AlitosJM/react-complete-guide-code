d1b8b6300d985ea28d6355ddd32f4df7c6bee546
AliTosJM <alitosjm@gmail.com>
2021-07-12 23:45:26 -0500

    aproach 1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const url = 'https://http-hook-default-rtdb.firebaseio.com/tasks.json';
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-2a442-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transformTask = useCallback((tasksObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+
+  }, []);
+
+  // const httpData = useHttp({url:url}, transformTask);
+  // const {isLoading, error, sendRequest: fetchTasks} = httpData;
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp(transformTask);
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://http-hook-default-rtdb.firebaseio.com/tasks.json'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+   // fetchTasks();
+    fetchTasks({url:url});
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
---
+++ src/hooks/use-http.js
@@ -0,0 +1,49 @@
+import {useState, useCallback} from 'react';
+// 'https://http-hook-default-rtdb.firebaseio.com/tasks.json'
+
+const useHttp = (applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const sendRequest = useCallback(async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+           requestConfig.url, {
+               method: requestConfig.method ? requestConfig.method: "GET",
+               headers: requestConfig.headers ? requestConfig.headers: {} ,
+               body: requestConfig.body ? JSON.stringify(requestConfig.body): null
+           }
+          
+        );
+  
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+  
+        const data = await response.json();
+  
+        // const loadedTasks = [];
+  
+        // for (const taskKey in data) {
+        //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+        // }
+  
+        // setTasks(loadedTasks);
+        applyData(data);
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
+      }
+      setIsLoading(false);
+    }, [applyData]);
+
+    return {
+        isLoading: isLoading,
+        error: error,
+        sendRequest: sendRequest
+    };
+
+};
+
+export default useHttp;
