2def02e052599fab1ab97e022d87cdf2d55974cf
AliTosJM <alitosjm@gmail.com>
2021-07-22 21:23:46 -0500

    2 slice

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 export const INCREMENT = "increment";
 
-const initialState = {counter:0, showCounter:true};
+const initialCounterState = {counter:0, showCounter:true};
 
 // const counterReducer = (state=initialState, action) => {
 //     if(action.type === INCREMENT){
@@ -39,7 +39,7 @@ const initialState = {counter:0, showCounter:true};
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
       increment(state) {
         state.counter++;
@@ -55,11 +55,30 @@ const counterSlice = createSlice({
       }
     }
   });
+
+const initialAuthState = {
+    isAuthenticated: false,
+};
+  
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+      login(state) {
+        state.isAuthenticated = true;
+      },
+      logout(state) {
+        state.isAuthenticated = false;
+        },
+    },
+});
   
 const store = configureStore({
-reducer: counterSlice.reducer
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
   
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
+  
 export default store;
+  
