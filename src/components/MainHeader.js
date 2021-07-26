033cd6dd48a1ee800887b6d57cd2d1cc7bd9b2ff
AliTosJM <alitosjm@gmail.com>
2021-07-26 15:20:08 -0500

    Route 3

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,19 @@
+import {Link, NavLink} from 'react-router-dom';
+import classes from './MainHeader.module.css';
+
 const MainHeader = () => {
-    return <header>
+    return <header className={classes.header}>
         <nav>
             <ul>
                 <li>
-                    <a href="/welcome">Welcome!</a>
+                    {/* <a href="/welcome">Welcome!</a> */}
+                    {/* <Link to="/welcome">Welcome!</Link> */}
+                    <NavLink activeClassName={classes.active} to="/welcome">Welcome!</NavLink>
                 </li>
                 <li>
-                    <a href="/products">Products!</a>
+                    {/* <a href="/products">Products!</a> */}
+                    {/* <Link to="/products">Products!</Link> */}
+                    <NavLink activeClassName={classes.active} to="/products">Products!</NavLink>
                 </li>
 
             </ul>
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,10 @@
+const ProductDetail = () => {
+    return(
+        <section>
+
+        </section>
+    );
+
+};
+
+export default ProductDetail;
